ef3794a4a2739d4c64348df19b50eec627093bb3
yunsarasak <rkddbstjr2@naver.com>
2023-09-26 23:19:43 +0900

    Solve boj1629, divide and conquer

--- boj/boj1629.py
+++ boj/boj1629.py
@@ -1,13 +1,19 @@
 A, B, C = list(map(int, input().split()))
 
 def power( _base:int, _exp:int ):
-    global A
-    global B
     global C
+    #print("base : ", _base, "_exp :", _exp)
     if _exp == 0:
         return 1
     elif _exp == 1:
-        return _base
+        return (_base % C)
     
+    k = power(_base, _exp//2)
+    #print(k)
     if _exp % 2 == 0:
-        return power( ... )
+        return (k * k) % C
+    else:
+        return (_base * (k * k)) % C
+
+
+print(power(A, B))
