c59420f180693ab974723a8719cbc52bdaea788d
yunsarasak <rkddbstjr2@naver.com>
2024-01-17 22:35:38 +0900

    Fix 1946.py

--- boj/boj1946.py
+++ boj/boj1946.py
@@ -1,37 +1,31 @@
-from collections import Counter
-test_case = int(input())
+import sys
+test_case = int(sys.stdin.readline().rstrip())
 
 while test_case:
     test_case-=1
 
-    num_applicant = int(input())
+    num_applicant = int(sys.stdin.readline().rstrip())
     applicant_line = list()
-    applicant_drop_array = [0 for _ in range(num_applicant)]
 
     for _ in range(num_applicant):
-        document_score, interview_score = list(map(int, input().split()))
+        document_score, interview_score = list(map(int, sys.stdin.readline().rstrip().split()))
         applicant_line.append([document_score, interview_score])
 
-    # print(applicant_line)
+    #print(applicant_line)
 
     sorted_line = sorted(applicant_line, key=lambda item:item[0])
-    # print(sorted_line)
+    #print(sorted_line)
 
-    for i in range(num_applicant):
-        for j in range(i+1, num_applicant):
-            # print("comapre", sorted_line[i][1] ,"<", sorted_line[j][1] )
-            if sorted_line[i][1] < sorted_line[j][1]:
-                # print("true")
-                applicant_drop_array[j] = 1
-    
-    drop_count = Counter(applicant_drop_array)
-    # print("drop_count:", drop_count)
-    
-    survivor_count = drop_count[0]
-    
+    # the first man always win in first match, so should be chosen.
+    num_of_chosen = 1
+    choice_threshold = sorted_line[0][1]
+    for i in range(1,num_applicant):
+        # then compare second score, if defeat in second match, he or she will be chosen
+        if choice_threshold > sorted_line[i][1]:
+            # the next threshold should be updated to current applicant
+            # so, next applicant defeat this applicant then be chosen because
+            # altough lose the first match, but win in second match. ( because they are sorted in ascending order )
+            choice_threshold = sorted_line[i][1]
+            num_of_chosen += 1
 
-    # print("survivor:",survivor_count)
-    # for i in range(num_applicant):
-    #     if applicant_drop_array[i] == 0:
-    #         print(sorted_line[i])
-    print(survivor_count)
+    print(num_of_chosen)
