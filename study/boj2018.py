707add7efb31381d849cba3a353228bc93b2aa74
Yun Seok Kang <yunseok966@telcoware.com>
2024-02-14 12:57:59 +0900

    timeout 2018

--- study/boj2018.py
+++ study/boj2018.py
@@ -10,3 +10,39 @@
 # 8, 
 # 9, 4+5
 # 10, 1+2+3+4
+
+N = int(input())
+
+answer = 0
+
+#이어지는 수의 첫번째 수, 즉 시작 숫자를 저장하기 위한 변수
+starting_number = 1
+
+#이어지는 수의 인덱스
+number_added = 0
+
+while starting_number != N:
+    sum = 0
+    number_added = 0
+
+    # 합이 입력을 넘어서면 리턴
+    while sum < N:
+        # 이어지는 수들을 차례로 더한다.
+        sum+= starting_number+number_added
+
+        # 합이 입력과 같다면, 조건을 만족하므로 가지수 +1
+        if sum == N:
+            answer += 1
+            #print("%d to %d"%(starting_number, starting_number+number_added))
+            break
+
+        # 아니라면 다음 수까지 더해본다
+        number_added += 1
+    
+    # 시작 하는 수를 1증가
+    starting_number+=1
+
+# N 하나만으로도 수열이므로 가지수 +1
+answer += 1
+
+print(answer)
